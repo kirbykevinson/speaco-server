2adf54e882886fe9123e8d92f2f5cb216b8dab7b
kirbykevinson <kirbykevinson@outlook.com>
2021-04-01 10:06:19 +0700

    Add basic event handling

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ class Chat {
 		this.port = port || "6942";
 		
 		this.socket = null;
+		
+		this.clients = [];
 	}
 	
 	run() {
@@ -17,6 +19,69 @@ class Chat {
 		this.socket.on("listening", () => {
 			console.log(`Server started on ${this.host}:${this.port}`);
 		});
+		
+		this.socket.on("connection", (client) => {
+			this.clients.push(client);
+			
+			client.on("close", () => {
+				this.clients.splice(this.clients.indexOf(client), 1);
+			});
+			client.on("error", (error) => {
+				this.error(client, error.message);
+			});
+			
+			client.on("message", (data) => {
+				let parsedEvent = null;
+				
+				try {
+					parsedEvent = JSON.parse(data);
+				} catch (error) {
+					this.error(client, "malformed client-sent event");
+					
+					return;
+				}
+				
+				if (!parsedEvent || typeof parsedEvent != "object") {
+					this.error("client, client-sent event is not an object");
+					
+					return;
+				}
+				
+				if (!("type" in parsedEvent)) {
+					this.error(client, "client-sent event without a type");
+					
+					return;
+				}
+				
+				this.recieveEvent(client, parsedEvent);
+			});
+		});
+	}
+	
+	error(client, message) {
+		this.sendEvent(client, "error", {
+			message: message
+		});
+		
+		client.terminate();
+	}
+	
+	sendEvent(client, type, data) {
+		if (!data || typeof data != "object") {
+			throw new TypeError("event must be an object");
+		}
+		
+		data.type = type;
+		
+		client.send(JSON.stringify(data));
+	}
+	recieveEvent(client, event) {
+		switch(event.type) {
+		default:
+			this.error(client, "illegal client-sent event type");
+			
+			break;
+		}
 	}
 }
 
